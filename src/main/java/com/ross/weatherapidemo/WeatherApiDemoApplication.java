214c551f8a3c35d377956eaf5d9d69b444ba0ffa
rossboyel <rossboylegla@gmail.com>
2023-05-15 19:19:30 +0100

    Initial depo creation

---
+++ src/main/java/com/ross/weatherapidemo/WeatherApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ross.weatherapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherApiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ross/weatherapidemo/WeatherApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ross.weatherapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
